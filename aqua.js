01e3d81ba7b85fcf729c9b909e6e92b3367215ee
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2025-02-19 12:09:32 -0600

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -9176,6 +9176,20 @@ if ((g_lname != null) && (checkin === null || checkin === '') &&  (keyid === nul
     console.log('string IS empty');
 }  
 
+if (g_report === 'activitylog') {
+      spinnow(3000);
+        //window.location.href = './activity/index.html';
+        //setTimeout("window.location.href = './activity/';", 3000);
+        var theTop=((screen.height/2)-(800/2))/2;
+        var theLeft=(screen.width/2)-(800/2);
+        var features = 'height=800,width=800,top='+theTop+',left='+theLeft+',toolbar=1,Location=0,Directories=0,Status=0,menubar=1,Scrollbars=1,Resizable=1';
+
+        window.open("./activity/index.html", "Activity", features);
+        setTimeout("window.location.reload();", 3000);
+} else {
+    console.log('string IS empty');
+}  
+
 if (g_report === 'labels') {
     getdateoflabels();
 } else {
